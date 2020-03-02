eeefecbc695c5bb3c3629d78410556362b238ac5
Kc Kai <kirstenckai@gmail.com>
2020-03-02 22:27:47 +0000

    Custom assertion function

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌❌Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Cohort", "Cohort"); // pass
+assertEqual("burgers", "fries"); // fail
+assertEqual(10, 10); // pass
+assertEqual(100, 34); // fail
