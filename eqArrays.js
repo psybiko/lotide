2019120d28a135cbc69bd2df4a28536d8568c97b
Kc Kai <kirstenckai@gmail.com>
2020-03-04 06:10:05 +0000

    Remove nested for loop

--- eqArrays.js
+++ eqArrays.js
@@ -8,30 +8,19 @@ const assertEqual = function (actual, expected) {
 
 };
 
-// const eqArrays = function (arr1, arr2) {
-//   if (arr1.length !== arr2.length)
-//   return false;
 
-//   for (let i = 0; i < arr1.length; i++) {
-//     if (arr1[i] !== arr2[i])
-//     return false;
-//   }
-//   return true;
-// };
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length != arr2.length)
-  return false;
-
-  for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      if (arr1[i] !== arr2[j]) {
-        return false
-      }
-    }
-    return true;
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length)
+    return false;
+  for (let i = arr1.length; i--;) {
+    if (arr1[i] !== arr2[i])
+      return false;
   }
+
+  return true;
 };
+
+
 
 // Tests
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
