6c093e502dc1574332372231bebadb0c5f39c8b5
Kc Kai <kirstenckai@gmail.com>
2020-03-04 03:30:38 +0000

    Add nested for loop

--- eqArrays.js
+++ eqArrays.js
@@ -8,15 +8,29 @@ const assertEqual = function (actual, expected) {
 
 };
 
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length)
+// const eqArrays = function (arr1, arr2) {
+//   if (arr1.length !== arr2.length)
+//   return false;
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i])
+//     return false;
+//   }
+//   return true;
+// };
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length != arr2.length)
   return false;
 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i])
-    return false;
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] !== arr2[j]) {
+        return false
+      }
+    }
+    return true;
   }
-  return true;
 };
 
 // Tests
