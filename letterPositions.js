484eb14ea953395adc209be150e2551d41bf9cfc
Kc Kai <kirstenckai@gmail.com>
2020-03-05 02:48:51 +0000

    Create two loops to create a key and value derived from string

--- letterPositions.js
+++ letterPositions.js
@@ -1,4 +1,4 @@
-// Check if two arrays equal each other
+//Check if two arrays equal each other
 const eqArrays = function (arr1, arr2) {
   let ifTrue = null;
   if (arr1.length !== arr2.length)
@@ -13,14 +13,35 @@ const eqArrays = function (arr1, arr2) {
   return ifTrue;
 };
 
-
-
-
-
-const letterPositions = function(sentence) {
+const letterPositions = function (sentence) {
   const results = {}
   // logic to update results here
+
+  for (let i = 0; i < sentence.length; i++) {
+    let currentLetter = sentence[i]
+    if (currentLetter !== ' ') {
+      results[currentLetter] = [];
+
+    }
+  }
+  for (let i = 0; i < sentence.length; i++) {
+    // key
+    let currentLetter = sentence[i];
+    results[currentLetter].push(i);
+
+  }
   return results;
 };
 
-console.log()
+
+
+const assertArraysEqual = function (a, b, expectation) {
+  if (eqArrays(a, b) === expectation) {
+    console.log(`✅✅✅Assertion Passed: ${a} ${b} ${expectation}`);
+  } else {
+    console.log(`❌❌❌Assertion Failed: ${a} ${b} ${expectation}`);
+  }
+
+};
+
+assertArraysEqual(letterPositions("hello").h, [0], true);
